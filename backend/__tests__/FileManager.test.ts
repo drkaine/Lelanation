f2eee3ce0a126f1502c8d3979bdff1e1613aa78b
drkaine <duretkevin@live.fr>
2024-10-24 14:00:39 +0000

    Add savePNG

--- backend/__tests__/FileManager.test.ts
+++ backend/__tests__/FileManager.test.ts
@@ -1,13 +1,13 @@
-import { saveJson } from "../src/FileManager";
+import { saveJSON, savePNG } from "../src/FileManager";
 import * as fs from "fs";
 import * as path from "path";
 
-describe("Create ", () => {
+describe("Create JSON", () => {
   it("files", () => {
     const jsonData: string = JSON.stringify({ key: "value" });
     const filePath = path.join(__dirname, "/files", "test.json");
 
-    saveJson(JSON.parse(jsonData), filePath);
+    saveJSON(JSON.parse(jsonData), filePath);
 
     expect(fs.existsSync(filePath)).toBe(true);
 
@@ -19,12 +19,38 @@ describe("Create ", () => {
     const filePath = path.join(__dirname, "/test", "test.json");
     const dirPath = path.join(__dirname, "/test");
 
-    saveJson(JSON.parse(jsonData), filePath);
+    saveJSON(JSON.parse(jsonData), filePath);
 
     expect(fs.existsSync(dirPath)).toBe(true);
 
     fs.unlinkSync(filePath);
 
+    fs.rmdirSync(dirPath);
+  });
+});
+
+describe("Create PNG", () => {
+  it("files", () => {
+    const pngData: Buffer = Buffer.from([137, 80, 78, 71, 13, 10, 26, 10]);
+    const filePath = path.join(__dirname, "/files", "test.png");
+
+    fs.writeFileSync(filePath, pngData);
+
+    expect(fs.existsSync(filePath)).toBe(true);
+
+    fs.unlinkSync(filePath);
+  });
+
+  it("folder", () => {
+    const pngData: Buffer = Buffer.from([137, 80, 78, 71, 13, 10, 26, 10]);
+    const filePath = path.join(__dirname, "/test", "test.png");
+    const dirPath = path.join(__dirname, "/test");
+
+    savePNG(pngData, filePath);
+
+    expect(fs.existsSync(dirPath)).toBe(true);
+
+    fs.unlinkSync(filePath);
     fs.rmdirSync(dirPath);
   });
 });
--- backend/src/FileManager.ts
+++ backend/src/FileManager.ts
@@ -1,6 +1,6 @@
 import * as fs from "fs";
 
-export function saveJson(data: JSON, path: string) {
+export function saveJSON(data: JSON, path: string) {
   const dirPath = path.substring(0, path.lastIndexOf("/"));
 
   if (!fs.existsSync(dirPath)) {
@@ -8,4 +8,14 @@ export function saveJson(data: JSON, path: string) {
   }
 
   fs.writeFileSync(path, JSON.stringify(data, null, 2));
+}
+
+export function savePNG(data: Buffer, path: string) {
+  const dirPath = path.substring(0, path.lastIndexOf("/"));
+
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath, { recursive: true });
+  }
+
+  fs.writeFileSync(path, data);
 }
