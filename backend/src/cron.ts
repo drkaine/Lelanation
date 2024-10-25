7e0847cdeacbf4a8fd1d7013f5dcbaf25bed39bf
drkaine <duretkevin@live.fr>
2024-10-25 14:22:41 +0000

    Refactoring cron

--- backend/src/cron.ts
+++ backend/src/cron.ts
@@ -3,40 +3,42 @@ import { DdragonAPI } from "../src/DdragonAPI";
 import { save } from "./FileManager";
 import { obtainKey } from "../src/JSONParser";
 
-async function getAllJSON() {
-  const targets: string[] = [
-    "data/fr_FR/champion.json",
-    "data/fr_FR/item.json",
-    "data/fr_FR/summoner.json",
-  ];
+const targets: string[] = [
+  "data/fr_FR/champion.json",
+  "data/fr_FR/item.json",
+  "data/fr_FR/summoner.json",
+];
 
-  const folderTarget = "frontend/src/assets/files/";
+const folderTarget = "frontend/src/assets/files/";
 
-  const ddragonAPI = new DdragonAPI();
-  let data;
+const ddragonAPI = new DdragonAPI();
 
+async function compilation() {
   targets.forEach(async (target: string) => {
-    data = await ddragonAPI.loadJson(target);
+    const data = await ddragonAPI.loadJson(target);
     const filename = target.split("/").pop();
     const filePath = path.join(__dirname, "../../" + folderTarget + filename);
 
     save(JSON.stringify(data), filePath);
   });
+}
 
+async function specific() {
   const championsName: string[] = await obtainKey(
     folderTarget + "/champion.json",
   );
 
   championsName.forEach(async (championName: string) => {
-    data = await ddragonAPI.loadJson(
+    const data = await ddragonAPI.loadJson(
       "data/fr_FR/champion/" + championName + ".json",
     );
-    const pathFile = path.join(
+    const filePath = path.join(
       __dirname,
       "../../" + folderTarget + "/champion/" + championName + ".json",
     );
-    save(JSON.stringify(data), pathFile);
+    save(JSON.stringify(data), filePath);
   });
 }
 
-getAllJSON();
+compilation();
+specific();
