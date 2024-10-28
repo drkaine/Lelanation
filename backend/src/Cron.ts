1102ad7b95fa9234a5114fad4f61e77cacac0195
drkaine <duretkevin@live.fr>
2024-10-28 10:24:23 +0000

    Add app and launch cron

---
+++ backend/src/Cron.ts
@@ -0,0 +1,41 @@
+import path from "path";
+import { DdragonAPI } from "./DdragonAPI";
+import { save } from "./FileManager";
+import { obtainKey } from "./JSONParser";
+
+const targets: string[] = [
+  "data/fr_FR/champion.json",
+  "data/fr_FR/item.json",
+  "data/fr_FR/summoner.json",
+];
+
+const folderTarget = "frontend/src/assets/files/";
+
+const ddragonAPI = new DdragonAPI();
+
+export async function compilation() {
+  targets.forEach(async (target: string) => {
+    const data = await ddragonAPI.loadJson(target);
+    const filename = target.split("/").pop();
+    const filePath = path.join(__dirname, "../../" + folderTarget + filename);
+
+    save(JSON.stringify(data), filePath);
+  });
+}
+
+export async function specific() {
+  const championsName: string[] = await obtainKey(
+    folderTarget + "/champion.json",
+  );
+
+  championsName.forEach(async (championName: string) => {
+    const data = await ddragonAPI.loadJson(
+      "data/fr_FR/champion/" + championName + ".json",
+    );
+    const filePath = path.join(
+      __dirname,
+      "../../" + folderTarget + "/champion/" + championName + ".json",
+    );
+    save(JSON.stringify(data), filePath);
+  });
+}
---
+++ backend/src/app.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cron from "node-cron";
+import { compilation, specific } from "./Cron";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+cron.schedule("0 0,12 * * *", () => {
+  console.log("Tâche cron exécutée à 00h00 et 12h00");
+  compilation();
+  specific();
+});
+
+app.listen(PORT, () => {
+  console.log(`Serveur en cours d'exécution sur le port ${PORT}`);
+});
