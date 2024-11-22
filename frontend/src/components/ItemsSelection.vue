de2996875878b47cc6116a31a9523985dbccf607
drkaine <duretkevin@live.fr>
2024-11-22 14:38:46 +0000

    Add tooltip and filter

--- frontend/src/components/ItemsSelection.vue
+++ frontend/src/components/ItemsSelection.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { ref, computed } from 'vue'
 import items from '@/assets/files/item.json'
 import version from '@/assets/files/lastVersion.json'
+import ItemTooltip from '@/components/ItemTooltip.vue'
 
 interface Item {
   name: string
@@ -41,11 +42,59 @@ interface Stats {
   [key: string]: number
 }
 
-const itemsData = ref<Item[]>([])
+const selectedTags = ref<string[]>([])
 
-onMounted(() => {
-  itemsData.value = Object.values(items.data)
+const toggleTag = (tag: string) => {
+  const index = selectedTags.value.indexOf(tag)
+  if (tag === 'all') {
+    selectedTags.value = []
+  } else if (index === -1) {
+    selectedTags.value.push(tag)
+  } else {
+    selectedTags.value.splice(index, 1)
+  }
+}
+
+const filteredItems = computed<Item[]>(() => {
+  if (selectedTags.value.length === 0) {
+    return Object.values(items.data).filter(
+      (item: Item) =>
+        item.maps['11'] === true &&
+        item.gold?.purchasable === true &&
+        item.consumed === undefined &&
+        item.gold.total > 0,
+    )
+  }
+  return Object.values(items.data).filter(
+    (item: Item) =>
+      selectedTags.value.every(tag => item.tags.includes(tag)) &&
+      item.maps['11'] === true &&
+      item.gold?.purchasable === true &&
+      item.gold.total > 0,
+  )
 })
+
+const itemsBoots = computed<Item[]>(() =>
+  filteredItems.value.filter((item: Item) => item.tags?.includes('Boots')),
+)
+
+const itemsBasic = computed<Item[]>(() =>
+  filteredItems.value.filter(
+    (item: Item) => item.depth === undefined && !item.tags?.includes('Boots'),
+  ),
+)
+
+const itemsEpic = computed<Item[]>(() =>
+  filteredItems.value.filter(
+    (item: Item) => item.depth === 2 && !item.tags?.includes('Boots'),
+  ),
+)
+
+const itemsLegendary = computed<Item[]>(() =>
+  filteredItems.value.filter(
+    (item: Item) => item.depth === 3 && !item.tags?.includes('Boots'),
+  ),
+)
 </script>
 
 <template>
@@ -60,39 +109,159 @@ onMounted(() => {
           </form>
         </div>
         <div data-v-aa396e7d="" class="filter">
-          <button data-v-27037513="" data-v-c8040147="" class="">
-            Attack Damage</button
-          ><button data-v-27037513="" data-v-c8040147="" class="">
-            Critical Strike</button
-          ><button data-v-27037513="" data-v-c8040147="" class="">
-            Attack Speed</button
-          ><button data-v-27037513="" data-v-c8040147="" class="">On-Hit</button
-          ><button data-v-27037513="" data-v-c8040147="" class="">
-            Armor Pen</button
-          ><button data-v-27037513="" data-v-c8040147="" class="">
-            Ability Power</button
-          ><button data-v-27037513="" data-v-c8040147="" class="">Mana</button
-          ><button data-v-27037513="" data-v-c8040147="" class="">
-            Magic Pen</button
-          ><button data-v-27037513="" data-v-c8040147="" class="">Health</button
-          ><button data-v-27037513="" data-v-c8040147="" class="">Armor</button
-          ><button data-v-27037513="" data-v-c8040147="" class="">
-            Magic Resist</button
-          ><button data-v-27037513="" data-v-c8040147="" class="">
-            Ability Haste</button
-          ><button data-v-27037513="" data-v-c8040147="" class="">
-            Movement</button
-          ><button data-v-27037513="" data-v-c8040147="" class="">
-            Life Steal &amp; Omnivamp
+          <button
+            data-v-27037513=""
+            data-v-c8040147=""
+            class=""
+            @click="toggleTag('all')"
+          >
+            Tous
+          </button>
+          <button
+            data-v-27037513=""
+            data-v-c8040147=""
+            class=""
+            @click="toggleTag('Damage')"
+          >
+            Attack Damage
+          </button>
+          <button
+            data-v-27037513=""
+            data-v-c8040147=""
+            class=""
+            @click="toggleTag('CriticalStrike')"
+          >
+            Critical Strike
+          </button>
+          <button
+            data-v-27037513=""
+            data-v-c8040147=""
+            class=""
+            @click="toggleTag('AttackSpeed')"
+          >
+            Attack Speed
+          </button>
+          <button
+            data-v-27037513=""
+            data-v-c8040147=""
+            class=""
+            @click="toggleTag('OnHit')"
+          >
+            On-Hit
+          </button>
+          <button
+            data-v-27037513=""
+            data-v-c8040147=""
+            class=""
+            @click="toggleTag('ArmorPenetration')"
+          >
+            Armor Pen
+          </button>
+          <button
+            data-v-27037513=""
+            data-v-c8040147=""
+            class=""
+            @click="toggleTag('SpellDamage')"
+          >
+            Ability Power
+          </button>
+          <button
+            data-v-27037513=""
+            data-v-c8040147=""
+            class=""
+            @click="toggleTag('Mana')"
+          >
+            Mana
+          </button>
+          <button
+            data-v-27037513=""
+            data-v-c8040147=""
+            class=""
+            @click="toggleTag('ManaRegen')"
+          >
+            Mana Regen
+          </button>
+          <button
+            data-v-27037513=""
+            data-v-c8040147=""
+            class=""
+            @click="toggleTag('MagicPenetration')"
+          >
+            Magic Pen
+          </button>
+          <button
+            data-v-27037513=""
+            data-v-c8040147=""
+            class=""
+            @click="toggleTag('Health')"
+          >
+            Health
+          </button>
+          <button
+            data-v-27037513=""
+            data-v-c8040147=""
+            class=""
+            @click="toggleTag('HealthRegen')"
+          >
+            Health Regen
+          </button>
+          <button
+            data-v-27037513=""
+            data-v-c8040147=""
+            class=""
+            @click="toggleTag('Armor')"
+          >
+            Armor
+          </button>
+          <button
+            data-v-27037513=""
+            data-v-c8040147=""
+            class=""
+            @click="toggleTag('SpellBlock')"
+          >
+            Magic Resist
+          </button>
+          <button
+            data-v-27037513=""
+            data-v-c8040147=""
+            class=""
+            @click="toggleTag('AbilityHaste')"
+          >
+            Ability Haste
+          </button>
+          <button
+            data-v-27037513=""
+            data-v-c8040147=""
+            class=""
+            @click="toggleTag('Movement')"
+          >
+            Movement
+          </button>
+          <button
+            data-v-27037513=""
+            data-v-c8040147=""
+            class=""
+            @click="toggleTag('LifeSteal')"
+          >
+            Life Steal
+          </button>
+          <button
+            data-v-27037513=""
+            data-v-c8040147=""
+            class=""
+            @click="toggleTag('SpellVamp')"
+          >
+            Omnivamp
           </button>
         </div>
-        <div data-v-aa396e7d="" class="group type-grid">Starter items</div>
+
+        <div data-v-aa396e7d="" class="group type-grid">Basic items</div>
         <div
           data-v-0ba05451=""
           data-v-8e0e60d7=""
           data-v-aa396e7d=""
           class="tooltip-wrap"
-          v-for="(item, index) in itemsData"
+          v-for="(item, index) in itemsBasic"
           :key="index"
         >
           <div data-v-de17e6dc="" data-v-0ba05451="" class="tooltip">
@@ -113,38 +282,24 @@ onMounted(() => {
                 {{ item.gold.total }}
               </div>
             </button>
+            <ItemTooltip
+              :item="{
+                image: { full: item.image.full },
+                name: item.name,
+                description: item.description,
+                colloq: item.colloq,
+                gold: {
+                  base: item.gold.base,
+                  total: item.gold.total,
+                  sell: item.gold.sell,
+                },
+                tags: item.tags,
+                // stats: item.strats,
+                plaintext: item.plaintext,
+              }"
+            />
           </div>
         </div>
-
-        <div data-v-aa396e7d="" class="divider"></div>
-        <div data-v-aa396e7d="" class="group type-grid">Basic items</div>
-        <div
-          data-v-0ba05451=""
-          data-v-8e0e60d7=""
-          data-v-aa396e7d=""
-          class="tooltip-wrap"
-        >
-          <div data-v-de17e6dc="" data-v-0ba05451="" class="tooltip">
-            <button
-              data-v-8e0e60d7=""
-              data-v-de17e6dc-s=""
-              class="type-grid thumb"
-              to="false"
-              replace="false"
-            >
-              <img
-                data-v-8e0e60d7=""
-                data-v-de17e6dc-s=""
-                class="img"
-                src="https://ddragon.leagueoflegends.com/cdn/${version}/img/item/1004.png"
-              /><!---->
-              <div data-v-8e0e60d7="" data-v-de17e6dc-s="" class="gold">
-                200
-              </div></button
-            ><!---->
-          </div>
-        </div>
-
         <div data-v-aa396e7d="" class="divider"></div>
         <div data-v-aa396e7d="" class="group type-grid">Boots items</div>
         <div
@@ -152,6 +307,8 @@ onMounted(() => {
           data-v-8e0e60d7=""
           data-v-aa396e7d=""
           class="tooltip-wrap"
+          v-for="(item, index) in itemsBoots"
+          :key="index"
         >
           <div data-v-de17e6dc="" data-v-0ba05451="" class="tooltip">
             <button
@@ -165,15 +322,30 @@ onMounted(() => {
                 data-v-8e0e60d7=""
                 data-v-de17e6dc-s=""
                 class="img"
-                src="https://ddragon.leagueoflegends.com/cdn/${version}/img/item/1001.png"
-              /><!---->
+                :src="`https://ddragon.leagueoflegends.com/cdn/${version}/img/item/${item.image.full}`"
+              />
               <div data-v-8e0e60d7="" data-v-de17e6dc-s="" class="gold">
-                300
-              </div></button
-            ><!---->
+                {{ item.gold.total }}
+              </div>
+            </button>
+            <ItemTooltip
+              :item="{
+                image: { full: item.image.full },
+                name: item.name,
+                description: item.description,
+                colloq: item.colloq,
+                gold: {
+                  base: item.gold.base,
+                  total: item.gold.total,
+                  sell: item.gold.sell,
+                },
+                tags: item.tags,
+                // stats: item.strats,
+                plaintext: item.plaintext,
+              }"
+            />
           </div>
         </div>
-
         <div data-v-aa396e7d="" class="divider"></div>
         <div data-v-aa396e7d="" class="group type-grid">Epic items</div>
         <div
@@ -181,6 +353,8 @@ onMounted(() => {
           data-v-8e0e60d7=""
           data-v-aa396e7d=""
           class="tooltip-wrap"
+          v-for="(item, index) in itemsEpic"
+          :key="index"
         >
           <div data-v-de17e6dc="" data-v-0ba05451="" class="tooltip">
             <button
@@ -194,12 +368,28 @@ onMounted(() => {
                 data-v-8e0e60d7=""
                 data-v-de17e6dc-s=""
                 class="img"
-                src="https://ddragon.leagueoflegends.com/cdn/${version}/img/item/3866.png"
-              /><!---->
+                :src="`https://ddragon.leagueoflegends.com/cdn/${version}/img/item/${item.image.full}`"
+              />
               <div data-v-8e0e60d7="" data-v-de17e6dc-s="" class="gold">
-                400
-              </div></button
-            ><!---->
+                {{ item.gold.total }}
+              </div>
+            </button>
+            <ItemTooltip
+              :item="{
+                image: { full: item.image.full },
+                name: item.name,
+                description: item.description,
+                colloq: item.colloq,
+                gold: {
+                  base: item.gold.base,
+                  total: item.gold.total,
+                  sell: item.gold.sell,
+                },
+                tags: item.tags,
+                // stats: item.strats,
+                plaintext: item.plaintext,
+              }"
+            />
           </div>
         </div>
 
@@ -210,6 +400,8 @@ onMounted(() => {
           data-v-8e0e60d7=""
           data-v-aa396e7d=""
           class="tooltip-wrap"
+          v-for="(item, index) in itemsLegendary"
+          :key="index"
         >
           <div data-v-de17e6dc="" data-v-0ba05451="" class="tooltip">
             <button
@@ -223,12 +415,28 @@ onMounted(() => {
                 data-v-8e0e60d7=""
                 data-v-de17e6dc-s=""
                 class="img"
-                src="https://ddragon.leagueoflegends.com/cdn/${version}/img/item/3867.png"
-              /><!---->
+                :src="`https://ddragon.leagueoflegends.com/cdn/${version}/img/item/${item.image.full}`"
+              />
               <div data-v-8e0e60d7="" data-v-de17e6dc-s="" class="gold">
-                400
-              </div></button
-            ><!---->
+                {{ item.gold.total }}
+              </div>
+            </button>
+            <ItemTooltip
+              :item="{
+                image: { full: item.image.full },
+                name: item.name,
+                description: item.description,
+                colloq: item.colloq,
+                gold: {
+                  base: item.gold.base,
+                  total: item.gold.total,
+                  sell: item.gold.sell,
+                },
+                tags: item.tags,
+                // stats: item.strats,
+                plaintext: item.plaintext,
+              }"
+            />
           </div>
         </div>
 
