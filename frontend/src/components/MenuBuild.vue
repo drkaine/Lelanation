26bd56fbd36a7fafefd54abac51ae6b94a17121f
drkaine <duretkevin@live.fr>
2024-12-06 15:14:32 +0000

    Fix the menu href

--- frontend/src/components/MenuBuild.vue
+++ frontend/src/components/MenuBuild.vue
@@ -1,23 +1,43 @@
+<script setup lang="ts">
+import { useStepStore } from '@/stores/stepStore'
+
+const stepStore = useStepStore()
+
+const next = (type: 'champion' | 'rune' | 'item') => {
+  stepStore.setStepSelection(type)
+}
+</script>
+
 <template>
   <div data-v-b6709614="" class="menu">
     <a
       data-v-b6709614=""
-      href="/build/champions"
       class="router-link-active router-link-exact-active done"
       aria-current="page"
+      @click="next('champion')"
     >
       Champions</a
     >
     <span data-v-b6709614="" class="arrow"></span>
-    <a data-v-b6709614="" href="/build/runes" :class="{}">
+    <a
+      data-v-b6709614=""
+      class="router-link-active router-link-exact-active done"
+      @click="next('rune')"
+    >
       <span data-v-b6709614="" class="long">
         Runes &amp; sort d'invocateur</span
       >
       <span data-v-b6709614="" class="short"> Runes </span>
     </a>
     <span data-v-b6709614="" class="arrow"></span>
-    <a data-v-b6709614="" href="/build/items" :class="{}"> Items</a>
+    <a
+      data-v-b6709614=""
+      class="router-link-active router-link-exact-active done"
+      @click="next('item')"
+    >
+      Items</a
+    >
     <span data-v-b6709614="" class="arrow"></span>
-    <a data-v-b6709614="" href="/build/info" class="done"> Info</a>
+    <a data-v-b6709614="" class="done"> Info</a>
   </div>
 </template>
