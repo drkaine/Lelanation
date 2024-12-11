94a47c5f49b795966eee2bdbd6dcc74deaf775b1
drkaine <duretkevin@live.fr>
2024-12-11 09:41:17 +0000

    Create api and use .env for port

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,37 @@
 import express from "express";
 import cron from "node-cron";
+import { save } from "./FileManager";
 import { compilation } from "./Cron";
+import cors from "cors";
+import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+const corsOptions = {
+  origin: "http://www.dev.lelandriva.darkaine.fr",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"],
+};
+
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 3500;
+
+app.use(express.json());
+
+app.post("/api/save/:filename", (req, res) => {
+  const filename = req.params.filename;
+  const data = req.body;
+
+  save(
+    JSON.stringify(data),
+    path.join(__dirname, "../../frontend/src/assets/build/" + filename),
+  );
+  res.sendStatus(200);
+});
 
 cron.schedule("0 0,12 * * *", () => {
   console.log("Tâche cron exécutée à 00h00 et 12h00");
