3975984c1787fc71c27c3dd9f6a0566d5719be44
drkaine <duretkevin@live.fr>
2024-12-23 21:43:47 +0000

    Add cd pipeline

---
+++ .github/.scripts/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+echo "Le script commence"
+cd ~/production/lelandriva/ &&
+
+# Pull la dernière version de l'application.
+echo "pull origin main"
+git pull
+
+echo "Le déploiement commence ..."
+
+pm2 restart all
+
+cd /backend/ && npm install
+
+cd ../frontend/ && npm install && npm run build
+
+echo "Déploiement terminé!"
