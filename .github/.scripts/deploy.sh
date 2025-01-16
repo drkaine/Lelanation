543e580c1ba695ed1ff4908b3c1549cd14158872
drkaine <duretkevin@live.fr>
2025-01-16 07:50:00 +0000

    Fix deploy script

--- .github/.scripts/deploy.sh
+++ .github/.scripts/deploy.sh
@@ -10,10 +10,13 @@ git pull
 
 echo "Le déploiement commence ..."
 
-pm2 restart all
+echo "install backend"
+cd backend/ && npm install
 
-cd /backend/ && npm install
-
+echo "install frontend"
 cd ../frontend/ && npm install && npm run build
+
+echo "restart pm2"
+pm2 restart all --update-env
 
 echo "Déploiement terminé!"
